5de7756c82f9c347bc6dbabeb88ba53c102e20a7
Shlenov <161601013+Shlenov@users.noreply.github.com>
2024-06-05 21:38:11 +0300

    Update Number№2.py

--- Number№2.py
+++ Number№2.py
@@ -1,21 +1,39 @@
+import sys
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
  
-tree = ET.parse('results.xml')
+
+if len(sys.argv) < 2:
+    Width = 8
+    Heiht = 5
+elif len(sys.argv) < 3: 
+    Width = sys.argv[1]
+    Heiht = 5
+else: 
+    Width = sys.argv[1]
+    Heiht = sys.argv[2]
+    
+a = [Width,Heiht]
+
+
+
+tree = ET.parse('results/results.xml')
 root = tree.getroot()
 x = []
 y = []
 for i in range(len(root)):
     for j in range(len(root[i])):
         if i == 0:
-            x.append(int(root[i][j].text))
+            x.append(float(root[i][j].text))
         if i == 1:
-            y.append(int(root[i][j].text))
+            y.append(float(root[i][j].text))
             
+print(x)
+print(y)
+ 
+plt.rcParams ['figure.figsize'] = a
 plt.plot(x, y)
 plt.title("График функции y = 100 * sqrt(abs(A-0.01*x*x))+0.01*abs(x+10)")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
-
-print(x)
-print(y)
